b7756d6b676c2ec3deaaf0f84c8b0294ea25622a
Adam Oswalt <adam.oswalt@gmail.com>
2016-04-25 10:30:04 -0500

    Convert buildTree function to take treeData object as argument

--- quiz.js
+++ quiz.js
@@ -1,15 +1,19 @@
 function tree() {
-	var height = document.getElementById("tree-height-input").value;
-	var character = document.getElementById("tree-character-input").value;
+	var treeData = {};
+	treeData.height = document.getElementById("tree-height-input").value;
+	treeData.character = document.getElementById("tree-character-input").value;
+	buildTree(treeData);
+}
 
-	if(height < 1 || character.length < 1) return;
+function buildTree(treeData) {
+	if(treeData.height < 1 || treeData.character.length < 1) return;
 
 	var treeString = "";
-	for (var i = 0; i < height; ++i) {
-		var spaces = Array(height - i + 1).join(' ');
-		var branch = Array(i + 1).join(character);
+	for (var i = 0; i < treeData.height; ++i) {
+		var spaces = Array(treeData.height - i + 1).join(' ');
+		var branch = Array(i + 1).join(treeData.character);
 
-		var row = spaces + branch + character + branch + spaces;
+		var row = spaces + branch + treeData.character + branch + spaces;
 		console.log(row);
 
 		treeString += row + "<br/>";
